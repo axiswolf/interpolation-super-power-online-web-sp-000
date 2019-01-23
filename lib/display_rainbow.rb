ff6afd16061fadf7569f331e2c0b770f0015cf51
Karen Vo <karenvo@usc.edu>
2019-01-23 00:45:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,5 +9,6 @@ def display_rainbow(colors)
   puts "I: #{colors[5]}"
   puts "V: #{colors[6]}"
 end
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 # rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 display_rainbow(rainbow)
