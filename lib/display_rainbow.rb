e1dd795adc07dfa560b774b1f6683c2c7a53b29e
Karen Vo <karenvo@usc.edu>
2019-01-23 00:21:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow
-  colors =["R: red"]
+  colors =["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
   puts "#{colors}"
 end
 puts display_rainbow
